357c368e293f801751f663070f232da0b5c6b795
GlauroJuliani <glauro.juliani@hotmail.com>
2018-01-08 00:27:30 -0200

    Init project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { version } from 'inferno';
+import Component from 'inferno-component';
+import './registerServiceWorker';
+import Logo from './logo';
+import './App.css';
+
+import Home from './pages/Home';
+
+import { Router, Route, IndexRoute } from 'inferno-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+const browserHistory = createBrowserHistory();
+
+const routes = (
+  <Router history={ browserHistory }>
+    <Route component={ App }>
+      <IndexRoute component={ Home }/>
+    </Route>
+  </Router>
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Route exact path="/Home" component={Home} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import { render } from 'inferno';
+import Component from 'inferno-component';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        HOME
+      </div>
+    );
+  }
+}
+
+export default Home;
